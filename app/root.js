3e3b542897a70706facde92a801e939aa83c5e1f
daskiro514 <luckybuzstar@gmail.com>
2022-02-10 09:55:58 +0900

    updated

---
+++ app/root.js
@@ -0,0 +1,57 @@
+import { Meta, Links, Scripts, useRouteData } from "@remix-run/react";
+import { Outlet } from "react-router-dom";
+
+import stylesUrl from "./styles/global.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+}
+
+export function loader() {
+  return { date: new Date() };
+}
+
+export default function App() {
+  let data = useRouteData();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+
+        <footer>
+          <p>This page was rendered at {data.date.toLocaleString()}</p>
+        </footer>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Oops!</title>
+      </head>
+      <body>
+        <div>
+          <h1>App Error</h1>
+          <pre>{error.message}</pre>
+          <p>
+            Replace this UI with what you want users to see when your app throws
+            uncaught errors. The file is at <code>app/root.tsx</code>.
+          </p>
+        </div>
+
+        <Scripts />
+      </body>
+    </html>
+  );
+}
